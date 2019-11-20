89c20c931e9c6910d2c7b55424737c329d8ce1fe
oasyslab\luca.lambiase <luca@lambia.it>
2019-11-20 18:39:07 +0100

    si va a casa

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="about">
-        <h1>This is NOT an about page</h1>
-        <CardsRecursive />
-        <Cards />
-    </div>
+    <v-container fluid class="pa-0" ref="pageTop">
+        <v-layout text-center wrap>
+            <h1>This is NOT an about page</h1>
+            <CardsRecursive />
+            <Cards />
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
